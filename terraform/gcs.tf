fdf7b57642e0abaa675f5b6e3af5a0ad94bf85de
Sylvio Pedroza <5503662+sylvioneto@users.noreply.github.com>
2022-11-25 18:16:31 -0300

    Update gcs.tf

--- terraform/gcs.tf
+++ terraform/gcs.tf
@@ -1,6 +1,5 @@
 resource "google_storage_bucket" "df_output" {
   name                        = "${var.project_id}-df-output"
-  des
   location                    = var.region
   uniform_bucket_level_access = true
   force_destroy               = true
